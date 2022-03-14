f0d8d3434a5a9cfa2078679ed610cf6698a6051b
Serly Ardila <82203586+serlyardilaa@users.noreply.github.com>
2022-03-14 14:20:12 +0700

    Add files via upload

---
+++ womens-day/womens-day-with-jquery/js/script.js
@@ -0,0 +1,18 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+$('.count').countUp({
+    delay: 10,
+    time: 1500
+});
